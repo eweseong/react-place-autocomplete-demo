145e7f6347b1d408a57b2b8b94748c52f579c146
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2023-09-05 22:04:10 +0800

    update CSS, server and preview configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd())],
+    },
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
 
   test: {
     setupFiles: ['./setupTests.ts'],
