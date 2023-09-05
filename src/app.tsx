36a3d627b641eff9ae76fd66ba99a4db53994e4b
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2023-09-05 19:25:25 +0800

    setup postcss configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
-import './App.css';
+import styles from './app.module.scss';
 
 function App() {
   return (
-    <>
+    <main className={styles.app}>
       <h1>Vite + React</h1>
-    </>
+    </main>
   );
 }
 
