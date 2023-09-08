e785b27bbc9b609e32dbcf7478cc3f6e53160cfb
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2023-09-08 22:59:47 +0800

    add repo name as base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: pkg.name,
+
   plugins: [react()],
 
   css: {
