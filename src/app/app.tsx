b93f20ed9c05bcfd9e3009f7d218121e1c81ba41
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2023-09-08 22:37:50 +0800

    refactor to Map component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,15 @@
-import { Spin } from 'antd';
-import { useCallback, useEffect, useState } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Skeleton } from 'antd';
+import { useState } from 'react';
 import { useScript } from 'usehooks-ts';
 import configs from '../configs';
+import Map from '../features/map/map';
 import PlaceAutocomplete from '../features/place-autocomplete/place-autocomplete';
 import styles from './app.module.scss';
 
 const { mapApiKey, mapCenter, mapId } = configs;
 
 export function App() {
-  // hooks
   // use JavaScript API instead of web service because AutoComplete API doesn't support CORS as mentioned here https://issuetracker.google.com/issues/35827564
   const apiStatus = useScript(
     `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&libraries=geocoding,places&callback=Function.prototype`,
@@ -16,76 +17,43 @@ export function App() {
       removeOnUnmount: false,
     }
   );
-
-  // local states
   const isAppReady = apiStatus === 'ready';
-  const [map, setMap] = useState<google.maps.Map>();
-  const [, setMarker] = useState<google.maps.Marker>();
+  const [marker, setMarker] = useState<google.maps.Marker>();
 
-  // utils
-  const replaceMarker = useCallback((newMarker: google.maps.Marker) => {
-    setMarker((prevMarker) => {
-      prevMarker?.setMap(null);
-      return newMarker;
-    });
-  }, []);
-
-  useEffect(() => {
-    async function initMap() {
-      // lazy load libraries
-      const { Map } = (await google.maps.importLibrary('maps')) as google.maps.MapsLibrary;
-      const map = new Map(document.getElementById('map') as HTMLElement, {
-        center: mapCenter,
-        disableDefaultUI: true,
-        mapId: mapId,
-        zoom: 15,
-      });
-      const marker = new google.maps.Marker({
-        map: map,
-        position: mapCenter,
-        title: 'My Location',
-      });
-      replaceMarker(marker);
-      setMap(map);
-    }
-
-    if (apiStatus === 'ready') {
-      initMap();
-    }
-  }, [apiStatus, replaceMarker]);
+  if (!isAppReady) {
+    return (
+      <Skeleton.Node className={styles.skeleton} active>
+        <LoadingOutlined className={styles.icon} spin />
+      </Skeleton.Node>
+    );
+  }
 
   return (
-    <Spin size="large" spinning={!isAppReady} tip="Loading...">
-      <main className={styles.app}>
-        <div className={styles.sticky}>
-          <PlaceAutocomplete
-            label="Search Places"
-            onSelect={(placeId) => {
-              if (map && placeId) {
-                new google.maps.Geocoder().geocode({ placeId }).then(({ results }) => {
-                  const [result] = results;
+    <main className={styles.app}>
+      <div className={styles.sticky}>
+        <PlaceAutocomplete
+          label="Search Places"
+          onSelect={(placeId) => {
+            if (placeId) {
+              new google.maps.Geocoder().geocode({ placeId }).then(({ results }) => {
+                const [result] = results;
 
-                  if (result) {
-                    const infowindow = new google.maps.InfoWindow();
-                    const marker = new google.maps.Marker({
-                      map,
+                if (result) {
+                  setMarker(
+                    new google.maps.Marker({
                       position: result.geometry.location,
-                    });
-                    map.setCenter(result.geometry.location);
-                    infowindow.setContent(result.formatted_address);
-                    infowindow.open(map, marker);
-                    replaceMarker(marker);
-                  } else {
-                    window.alert('No results found');
-                  }
-                });
-              }
-            }}
-          />
-        </div>
-        <div id="map" className={styles.map}></div>
-      </main>
-    </Spin>
+                    })
+                  );
+                } else {
+                  window.alert('No results found');
+                }
+              });
+            }
+          }}
+        />
+      </div>
+      <Map center={mapCenter} id={mapId} marker={marker ?? new google.maps.Marker({ position: mapCenter })} />
+    </main>
   );
 }
 
